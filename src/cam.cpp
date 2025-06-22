12cfcbbd5ea4fb6dfb2460f60b953903d2e24d77
Esteban Campos <este0111@hotmail.com>
2025-06-22 05:02:20 -0600

    change c++ fifo logic to only read char directly

--- src/cam.cpp
+++ src/cam.cpp
@@ -9,16 +9,16 @@
 
 #include "cam.hpp"
 
-#define CAM_ERR        -1
-#define GESTURE_NOHAND  0
-#define GESTURE_CLOSED  1
-#define GESTURE_OPENED  2
+#define GESTURE_NOHAND  '0'
+#define GESTURE_CLOSED  '1'
+#define GESTURE_OPENED  '2'
+#define CAM_ERR         '3'
 
 static const char* GESTURE_FILE = "/tmp/gesture.txt";
 
 static SIGNAL_TYPE current_sound_selected;
 
-static int read_fifo_gesture() {
+static char read_fifo_gesture() {
     int fd = open(GESTURE_FILE, O_RDONLY);
     if (fd == -1) {
         return CAM_ERR;
@@ -26,14 +26,13 @@ static int read_fifo_gesture() {
 
     flock(fd, LOCK_SH);  // shared lock
 
-    char buffer[64];
-    ssize_t len = read(fd, buffer, sizeof(buffer) - 1);
-    buffer[(len > 0) ? len : 0] = '\0';
+    char buffer[4];
+    read(fd, buffer, sizeof(buffer) - 1);
 
     flock(fd, LOCK_UN);  // unlock
     close(fd);
 
-    return buffer[0] - '0';
+    return buffer[0];
 }
 
 void init_cam() {
@@ -42,28 +41,28 @@ void init_cam() {
 }
 
 void cam_check_gesture() {
-    int gesture = read_fifo_gesture();
+    char gesture = read_fifo_gesture();
     switch (gesture) {
-        case CAM_ERR:
-            std::cout << "Couldn't open the camera FIFO!" << std::endl;
-            break;
-
         case GESTURE_NOHAND:
             // just silently skip
             break;
-
+        
         case GESTURE_CLOSED:
             if (current_sound_selected == KS_e) {
                 current_sound_selected = WAVE_e;
                 change_sound_type(current_sound_selected);
             }
             break;
-
+        
         case GESTURE_OPENED:
             if (current_sound_selected == WAVE_e) {
                 current_sound_selected = KS_e;
                 change_sound_type(current_sound_selected);
             }
+            break;
+
+        case CAM_ERR:
+            std::cout << "Couldn't open the camera FIFO!" << std::endl;
             break;
         
         default:
