db3d706d8c2ac0be63b478b09095deb0a5ef64c1
Esteban Campos <este0111@hotmail.com>
2025-06-22 02:56:55 -0600

    add minimum contour area to gesture test

--- demos/gestures/gesture_console.py
+++ demos/gestures/gesture_console.py
@@ -34,31 +34,35 @@ while running:
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     gesture = "No hand detected"
+    MIN_CONTOUR_AREA = 5000
 
     if contours:
         max_contour = max(contours, key=cv2.contourArea)
-        hull = cv2.convexHull(max_contour, returnPoints=False)
-        if hull is not None and len(hull) > 3:
-            defects = cv2.convexityDefects(max_contour, hull)
-            if defects is not None:
-                finger_count = 0
-                for i in range(defects.shape[0]):
-                    s, e, f, d = defects[i, 0]
-                    start = tuple(max_contour[s][0])
-                    end = tuple(max_contour[e][0])
-                    far = tuple(max_contour[f][0])
-                    a = np.linalg.norm(np.array(start) - np.array(end))
-                    b = np.linalg.norm(np.array(far) - np.array(start))
-                    c = np.linalg.norm(np.array(far) - np.array(end))
-                    angle = np.arccos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
+        if cv2.contourArea(max_contour) > MIN_CONTOUR_AREA:
+            hull = cv2.convexHull(max_contour, returnPoints=False)
+            if hull is not None and len(hull) > 3:
+                defects = cv2.convexityDefects(max_contour, hull)
+                if defects is not None:
+                    finger_count = 0
+                    for i in range(defects.shape[0]):
+                        s, e, f, d = defects[i, 0]
+                        start = tuple(max_contour[s][0])
+                        end = tuple(max_contour[e][0])
+                        far = tuple(max_contour[f][0])
+                        a = np.linalg.norm(np.array(start) - np.array(end))
+                        b = np.linalg.norm(np.array(far) - np.array(start))
+                        c = np.linalg.norm(np.array(far) - np.array(end))
+                        if b * c == 0:
+                            continue  # avoid division by zero
+                        angle = np.arccos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
 
-                    if angle <= 90:
-                        finger_count += 1
+                        if angle <= 90:
+                            finger_count += 1
 
-                if finger_count >= 4:
-                    gesture = "Open Hand"
-                else:
-                    gesture = "Closed Fist"
+                    if finger_count >= 4:
+                        gesture = "Open Hand"
+                    else:
+                        gesture = "Closed Fist"
 
     print(f"Detected Gesture: {gesture}")
     time.sleep(0.5)
