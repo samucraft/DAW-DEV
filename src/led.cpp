68363c7fddf10b155380a8172c98f68ebe8b65b3
Esteban Campos <este0111@hotmail.com>
2025-06-24 04:03:13 -0600

    Add scale logic

--- src/led.cpp
+++ src/led.cpp
@@ -13,6 +13,7 @@
 #define LED_BRIGHTNESS 3
 #define LED_COLOR      0xF4430D
     #define LED_COLOR_BLK   0x000000
+    #define LED_COLOR_SCALE 0x4c1403
     #define LED_COLOR_SUG1  0x00FF00
     #define LED_COLOR_SUG2  0x0000FF
 #define LED_COUNT      18
@@ -63,6 +64,19 @@ static uint8_t key_leds[] = {
 
 static uint8_t last_sug1, last_sug2;
 
+static uint8_t scale[] = {1, 4, 6, 7, 11};
+
+static void light_off_led(uint8_t index) {
+    for (const auto& scale_key : scale) {
+        if (scale_key == index) {
+            ledstring.channel[0].leds[index] = LED_COLOR_SCALE;
+            return;
+        }
+    }
+
+    ledstring.channel[0].leds[index] = LED_COLOR_BLK;
+}
+
 uint8_t init_led() {
     if (ws2811_init(&ledstring) != WS2811_SUCCESS) {
         std::cout << "ws2811_init failed" << std::endl;
@@ -75,6 +89,9 @@ uint8_t init_led() {
     for (int i = LED_DISP_CNT; i < LED_COUNT; ++i) {
         ledstring.channel[0].leds[i] = LED_COLOR_BLK;
     }
+    for (const auto& scale_key : scale) {
+        ledstring.channel[0].leds[scale_key] = LED_COLOR_SCALE;
+    }
     ws2811_render(&ledstring);
 
     return E_LED_SUCC;
@@ -86,9 +103,13 @@ void cleanup_led() {
 
 void set_led(uint8_t index, bool state) {
     uint8_t  led_i = key_leds[index];
-    uint32_t color = state ? LED_COLOR : LED_COLOR_BLK;
 
-    ledstring.channel[0].leds[led_i] = color;
+    if (!state) {
+        light_off_led(led_i);
+    } else {
+        ledstring.channel[0].leds[led_i] = LED_COLOR;
+    }
+
     ws2811_render(&ledstring);
 }
 
