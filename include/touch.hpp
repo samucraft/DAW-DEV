c81b91769e5bdef1453ef86fe4aa8621a0183159
Esteban Campos <este0111@hotmail.com>
2025-06-23 21:29:05 -0600

    Fix logic on touch module

--- include/touch.hpp
+++ include/touch.hpp
@@ -1,24 +1,8 @@
 #ifndef DAW_TOUCH_H
 #define DAW_TOUCH_H
 
-// Key states
-#define TOUCH_UNPRESSED 0
-#define TOUCH_PRESSED   1
-
-typedef struct touch
-{
-    uint8_t pin;
-    uint8_t state;
-} touch;
-
 // Initialize keys
 uint8_t init_touch();
-
-// Set key state
-void set_touch(touch *touch, uint8_t state);
-
-// Get key state
-uint8_t get_touch(touch *touch);
 
 // Main loop routine for keys
 void loop_touch();
--- src/touch.cpp
+++ src/touch.cpp
@@ -8,17 +8,26 @@
 #define TOUCH_SUCCESS 0
 #define TOUCH_INITERR 1
 
-// Defines the max number of keys to load
+// Defines the max number of keys to load (must support up to 5 in the future)
 #define MAX_TOUCH 3
 
+typedef struct touch
+{
+    uint8_t pin;
+    uint8_t state;
+} TOUCH;
+
 static touch touches[] = {
-    {0 , false},
+    {7, false},
+    {0, false},
+    {2, false},
     {3, false},
-    {7, false}
+    {4, false}
 };
 
-// Update internal key state
-void update_touch(touch *touch, uint8_t state);
+static uint8_t get_touch(TOUCH *touch) {
+    return digitalRead(touch->pin);
+}
 
 uint8_t init_touch() {
     std::cout << "  * initializing touches ...\n";
@@ -28,21 +37,11 @@ uint8_t init_touch() {
     }
 
     for (size_t i = 0; i < MAX_TOUCH; i++) {
-        pinMode(touches[i].pin, OUTPUT);
-        set_touch(&touches[i], TOUCH_UNPRESSED);
+        pinMode(touches[i].pin, INPUT);
     }
 
     std::cout << " Touch Success!\n";
     return TOUCH_SUCCESS;
-}
-
-void set_touch(touch *touch, uint8_t state) {
-    update_touch(touch, state);
-    digitalWrite(touch->pin, state);
-}
-
-uint8_t get_touch(touch *touch) {
-    return digitalRead(touch->pin);
 }
 
 void loop_touch() {
@@ -50,9 +49,10 @@ void loop_touch() {
     for (size_t i = 0; i < MAX_TOUCH; i++) {
         tmp_state = get_touch(&touches[i]);
         if (tmp_state != touches[i].state) {
-            update_touch(&touches[i], tmp_state);
+            touches[i].state = tmp_state;
             std::cout << "Touch " << static_cast<int>(i) << " changed to "
                       << static_cast<int>(tmp_state) <<"!\n";
+
             if (tmp_state) {
                 trigger_sample(i);
             }
@@ -60,6 +60,6 @@ void loop_touch() {
     }
 }
 
-void update_touch(touch *touch, uint8_t state) {
+void update_touch(TOUCH *touch, uint8_t state) {
     touch->state = state;
 }
